ae7ff2fae1cc086065b8d5d77fa823a064935610
grant <xw2sy@163.com>
2017-07-17 16:58:45 +0800

    add auto mapping test

---
+++ src/main/java/cn/hn/java/summer/springnctest/service/IAutoMapping.java
@@ -0,0 +1,24 @@
+package cn.hn.java.summer.springnctest.service;
+
+import cn.hn.java.summer.springnc.annotation.AutoController;
+import cn.hn.java.summer.springnc.annotation.ExcludeMapping;
+import cn.hn.java.summer.springnc.annotation.RequestMapping;
+
+/**
+ * Created by xw2sy on 2017-07-17.
+ */
+@AutoController
+public interface IAutoMapping {
+
+    //mapped to "/one"
+    String one();
+
+    @ExcludeMapping
+    String two();
+
+    //mapped to "/three"
+    String three(String msg);
+
+    @RequestMapping("/imFour")
+    String four();
+}
